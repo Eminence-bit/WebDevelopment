79566f567c2d9ee1ca9ac913c7d1de97831b6c98
Prajyoth Nani <naniprajyoth123@gmail.com>
2024-04-06 20:43:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function bar1()
+{
+     window.location.href="https://www.instagram.com/prajyoth.4287/"
+}
+function bar2(){
+    window.location.href="https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSKkVLjNxdxsQHvmjvQQNsrqwPCNsJNQmhWcnKdfdrGpJJgWPXwdrMmGSGZhTtqSvPSnqDnD"
+}
+function bar3(){
+    window.location.href="https://github.com/Eminence-bit"
+}
